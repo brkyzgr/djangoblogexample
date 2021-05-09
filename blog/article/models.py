3c37c344f300fb3bff9d6b0828c48d39c7a4f709
Berkay Özgür <71180517+brkyzgr@users.noreply.github.com>
2021-05-09 04:27:45 +0300

    Update models.py

--- blog/article/models.py
+++ blog/article/models.py
@@ -3,17 +3,17 @@ from ckeditor.fields import RichTextField
 # Create your models here.
 
 class Article(models.Model):
-    author = models.ForeignKey("auth.User",on_delete = models.CASCADE,verbose_name="Yazar") # Bunla user tablosuna işaret ediyor. on_delete= models.CASCADE ise user silindimi ona iat olan article ların silinmesini sağlar
-    title = models.CharField(max_length=50,verbose_name="Başlık") # Bu model içindeki charfield ı kullanır.
+    author = models.ForeignKey("auth.User",on_delete = models.CASCADE,verbose_name="Yazar") 
+    title = models.CharField(max_length=50,verbose_name="Başlık") 
     content = RichTextField()
-    created_date = models.DateTimeField(auto_now_add = True,verbose_name="Oluşturulma Tarihi") # Burası tarih alanı için . auto_now_add = True --> bu o anki tarihi created date de atar.
-    article_image = models.FileField(blank=True,null=True,verbose_name="Makaleye Fotoğraf Ekleyin") # null = true yazmamızın sebebi bu alan hem boş hem null olabilir.
-    def __str__(self): # Bunun sayesinde title bilgisi gözükecek
+    created_date = models.DateTimeField(auto_now_add = True,verbose_name="Oluşturulma Tarihi") 
+    article_image = models.FileField(blank=True,null=True,verbose_name="Makaleye Fotoğraf Ekleyin") 
+    def __str__(self): 
         return self.title
     class Meta:
-        ordering = ['-created_date'] # Bu şekilde en son eklenen makale ilk başta gösterir.
+        ordering = ['-created_date']
 class Comment(models.Model):
-    article = models.ForeignKey(Article,on_delete=models.CASCADE,verbose_name="Makale",related_name="comment") # related_name --> article ların commentlerini almamızı sağlar
+    article = models.ForeignKey(Article,on_delete=models.CASCADE,verbose_name="Makale",related_name="comment") 
     comment_author = models.CharField(max_length=50,verbose_name="İsim")
     comment_content = models.CharField(max_length=200,verbose_name="Yorum")
     comment_date = models.DateTimeField(auto_now_add=True)
