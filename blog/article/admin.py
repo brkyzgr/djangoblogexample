f88e4eb264fcef9382d5e7320ed17076ff815971
Berkay Özgür <71180517+brkyzgr@users.noreply.github.com>
2021-05-09 04:25:50 +0300

    Update admin.py

--- blog/article/admin.py
+++ blog/article/admin.py
@@ -1,17 +1,14 @@
 from django.contrib import admin
-from .models import Article,Comment # Bu şekilde modeldeki article dahil ettik
+from .models import Article,Comment 
 
-admin.site.register(Comment) # Bu şekilde commentti kaydettik.
-
-# Register your models here.
-# admin.site.register(Article) # Bu şekilde article ı admin panelişnde gösterdik.
+admin.site.register(Comment) 
 
 @admin.register(Article)
-class ArticleAdmin(admin.ModelAdmin): # Burada admin içindeki modeladminden türettik
-    list_display = ["title","author","created_date"] # Bu şekilde articlen yazar ve başlığını gömsterdik.
-    list_display_links = ["title","created_date"] # Bu şekilde linke çevirdik.
-    search_fields = ["title"] # Bu şekilde sadece title bilgisine göre arama motoru oluşturulur.
-    list_filter = ["created_date"] # Bu şekilde gün ve ay bilgisine görre makale oluşturulma tarihlerini gösteren kısım oluşturur.
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ["title","author","created_date"] 
+    list_display_links = ["title","created_date"] 
+    search_fields = ["title"] 
+    list_filter = ["created_date"] 
     
-    class Meta: # Meta olmak zorunda 
-        model = Article # Bu şekilde articleadmin ile article birleştirdik
+    class Meta: 
+        model = Article 
